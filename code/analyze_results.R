e21ed74d613e9e661d340d6f169413ed68b69246
ksuelflo <134102709+ksuelflo@users.noreply.github.com>
2024-08-30 10:39:43 -0500

    Summary list updated

--- code/analyze_results.R
+++ code/analyze_results.R
@@ -1,16 +1,13 @@
 
-df <- readRDS("../Results/in_progress_summary_list.rds")
+df <- readRDS("../Results/in_progress_summary_list2.rds")
 
 df <- bind_rows(df)
 
 # Calculate "TRUE NMR, IMR, U5MR" from lnorm_mean and lnorm_sd
 plnorm(60, mean = 12, sd = exp(1.9))
 
-compute_plnorm <- function(q, mean, sd) {
-  plnorm(q, mean, sd)
-}
 
-df %>% 
+coverage_df <- df %>% 
   filter(model != "surv_synth_weibull") %>%
   mutate(true_u5mr = plnorm(60, mean = lnorm_mean, sd = exp(lnorm_sd)),
          true_imr = plnorm(12, mean = lnorm_mean, sd = exp(lnorm_sd)),
@@ -18,17 +15,53 @@ df %>%
   mutate(u5mr_covered = ifelse(U5MR_lower <= true_u5mr & U5MR_upper >= true_u5mr, 1, 0),
          nmr_covered = ifelse(NMR_lower <= true_nmr & NMR_upper >= true_nmr, 1, 0),
          imr_covered = ifelse(IMR_lower <= true_imr & IMR_upper >= true_imr, 1, 0)) %>%
-  group_by(model, sample_size, period, proportion) %>%
+  group_by(model, sample_size, proportion) %>%
   summarize(u5mr_coverage = mean(u5mr_covered, na.rm = TRUE),
             imr_coverage = mean(imr_covered, na.rm = TRUE),
             nmr_coverage = mean(nmr_covered, na.rm = TRUE)) %>%
   pivot_longer(cols = u5mr_coverage:nmr_coverage,
                names_to = "Estimate",
-               values_to = "Value") %>%
-  ggplot(aes(period, Value, col = model, shape = factor(proportion))) +
+               values_to = "Value")%>%
+  mutate(sample_size = as.factor(sample_size))
+
+
+#Labels for facet wrap
+labels_wrap <- c(`imr_coverage` = "IMR", `nmr_coverage` = "NMR", `u5mr_coverage` = "U5MR")
+
+#Main coverage plot
+coverage_df%>%
+  filter(Value != 0)%>%
+  # filter(model == "logquad_adj")%>%
+  ggplot(aes(x = as.factor(sample_size), y = Value, col = model, shape = factor(proportion))) +
   geom_point() +
   geom_hline(yintercept = 0.95) +
-  facet_wrap(~Estimate, scales = "free")
+  facet_wrap(~Estimate, scales = "free", labeller = as_labeller(labels_wrap))+
+  theme_minimal()+
+  labs(x = "Sample Size", y = "Coverage", shape = "Proportion", col = "Model",
+       title = "Coverage of NMR, IMR, and U5MR Estimates Compared to the True Value",
+       subtitle = "Shown are various proportions of heaping at 12 months, and models used to estimate NMR, IMR, and U5MR")
+
+coverage_df%>%
+  filter(model == "discrete_hazards")%>%
+  ggplot(aes(x = proportion, y = Value, col = sample_size))+
+  geom_point()+
+  geom_hline(yintercept = .95, linetype = "dashed")+
+  facet_wrap(~Estimate)+
+  theme_minimal()
+
+y_12_19 <- 1 - plnorm(1:60, meanlog = 12, sdlog = exp(1.9))
+y_20_23 <- 1 - plnorm(1:60, meanlog = 20, sdlog = exp(2.3))
+y_15_21 <- 1 - plnorm(1:60, meanlog = 15, sdlog = exp(2.1))
+y_15_23 <- 1 - plnorm(1:60, meanlog = 15, sdlog = exp(2.3))
+
+
+df2 <- df%>%
+  pivot_longer(cols = NMR:U5MR_upper, names_to = c("estimate", "lower", "upper"), names_pattern = "(^[A-Z0-9]+$)(lower$)(upper$)",
+               values_to = "value")
+View(df2)
+
+string <- c("NMR", "U5MR", "IMR", "NMR_lower", "U5MR_lower", "IMR_lower", "NMR_upper", "U5MR_upper", "IMR_upper")
+str_extract_all(string, "lower$")
 
 # Fix sample sizes
 # Remove logquad points from u5mr coverage plot
@@ -36,5 +69,5 @@ df %>%
 # figure out how to best incorporate low_range, high_range plot without being overwhelming
 # get rid of grouping by period, replace with sample size on x-axis
 # make legend, axis labels, etc. look nice
- 
+
 
--- code/results.R
+++ code/results.R
@@ -30,12 +30,12 @@ for (i in seq_along(summary_folders)){
       mutate(seed = str_extract(summaries[j], pattern = "(?<=seed=)\\d*"),
              lnorm_mean = as.numeric(str_extract(summaries[j], pattern = "(?<=lnormmean=)\\d*")),
              lnorm_sd = as.numeric(str_extract(summaries[j], pattern = "(?<=lnormsd=)\\d*"))/10,
-             sample_size = as.numeric(str_extract(summaries[j], pattern = "(?<=sample_size=)\\d*")),
+             sample_size = as.numeric(str_extract(summaries[j], pattern = "(?<=samplesize=)\\d*")),
              low_range = as.numeric(str_extract(summaries[j], pattern = "(?<=range=)\\d")),
              high_range = as.numeric(str_extract(summaries[j], pattern = "(?<=range=\\d)\\d*")),
              proportion = as.numeric(str_extract(summaries[j], pattern = "(?<=proportion=)\\d*"))/10)
+    summary$model[21:25] <- "logquad_adj"
     container[[j]] <- summary
-    
   }
   #Combining all the seeds into one df
   container <- bind_rows(container)
@@ -45,6 +45,5 @@ for (i in seq_along(summary_folders)){
   
 }
 
-
-
+saveRDS(big_container, "../Results/in_progress_summary_list2.rds")
 
